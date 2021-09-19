747ce4a66e44f85d88d5af09a4760f76b6a41ef6
vijay-jaisankar <vijayjaisankar.vj@gmail.com>
2021-09-19 20:53:37 +0530

    Added introductory files explained in session 1.

---
+++ java/session1/HelloAgain.java
@@ -0,0 +1,13 @@
+public class HelloAgain {
+
+	public static void main(String[] args) {
+		for (int i = 1; i <= 10; i++){
+			System.out.println("Hello " + i);
+			if(i==2)
+			{
+				System.out.println("yo boi");
+			}
+	}
+}
+
+}
